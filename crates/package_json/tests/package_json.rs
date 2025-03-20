bfdf7998df165f62ce55ccfdab08f239b465c4d1
ityuany <519495771@qq.com>
2025-03-20 14:29:37 +0800

    refactor(tests): 更新 package_json 库导入路径并修改实例化方法
    - 更改导入路径为 doctor_package_json 以匹配库的重命名
    - 使用新的 PackageJson::new 方法替代 try_from 进行对象创建

--- crates/package_json/tests/package_json.rs
+++ crates/package_json/tests/package_json.rs
@@ -1,11 +1,10 @@
 use std::{collections::HashMap, path::PathBuf};
 
-use package_json::package_json::PackageJson;
+use doctor_package_json::package_json::PackageJson;
 
 #[test]
 fn test_package_json() {
-  let package_json =
-    PackageJson::try_from(PathBuf::from("./tests/fixtures/package_1.json")).unwrap();
+  let package_json = PackageJson::new(PathBuf::from("./tests/fixtures/package_1.json")).unwrap();
   assert_eq!(package_json.name, Some("test".to_string()));
   assert_eq!(package_json.version, Some("1.0.0".to_string()));
   assert_eq!(package_json.private, Some(true));
