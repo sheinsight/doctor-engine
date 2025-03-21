0fcf8759b80f413cfae992734d751e17f258781e
ityuany <519495771@qq.com>
2025-03-21 14:07:32 +0800

    refactor(lib.rs): 重构模块导出方式
    将多个模块的直接导出改为通过显式的模块定义进行导出。这样可以使代码结构更清晰，模块的功能更加明确。

--- crates/doctor/src/lib.rs
+++ crates/doctor/src/lib.rs
@@ -1,6 +1,23 @@
-pub use doctor_ext;
-pub use doctor_lint;
-pub use doctor_node;
-pub use doctor_npmrc;
-pub use doctor_package_json;
-pub use doctor_walk_parallel;
+pub mod ext {
+  pub use doctor_ext::*;
+}
+
+pub mod lint {
+  pub use doctor_lint::*;
+}
+
+pub mod node {
+  pub use doctor_node::*;
+}
+
+pub mod npmrc {
+  pub use doctor_npmrc::*;
+}
+
+pub mod package_json {
+  pub use doctor_package_json::*;
+}
+
+pub mod walk_parallel {
+  pub use doctor_walk_parallel::*;
+}
