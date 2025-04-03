f2e0d7f945cd49b6234ca59d0421f283f4684d45
ityuany <519495771@qq.com>
2025-04-03 11:35:01 +0800

    refactor(validator): 移除不必要的导入以简化代码结构
    - 从 validator 模块中删除了 PackageJsonValidator、ValidateName 和 ValidatePackageManager 的单独导入，改为使用通配符导入，提升代码的简洁性。

--- crates/doctor/src/lib.rs
+++ crates/doctor/src/lib.rs
@@ -15,9 +15,7 @@ pub mod validator {
   pub use doctor_npmrc::validator::NpmrcValidator;
 
   pub use doctor_package_json::error::PackageJsonValidatorError;
-  pub use doctor_package_json::validator::PackageJsonValidator;
-  pub use doctor_package_json::validator::ValidateName;
-  pub use doctor_package_json::validator::ValidatePackageManager;
+  pub use doctor_package_json::validator::*;
 }
 
 pub mod walk_parallel {
