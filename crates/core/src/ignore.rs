17dd4cac93b65b0d7c9676c5dea4104786f66849
ityuany <519495771@qq.com>
2025-05-28 11:34:15 +0800

    更新 Cargo.toml 和 Cargo.lock 文件以添加 serde 依赖；新增 ignore.rs 文件，定义 Ignore 结构体及其相关实现，用于处理忽略模式，提高代码可读性和维护性。

---
+++ crates/core/src/ignore.rs
@@ -0,0 +1,52 @@
+use std::ops::{Deref, DerefMut};
+
+use serde::{Deserialize, Serialize};
+
+pub const IGNORE_PATTERNS: [&str; 17] = [
+  "**/node_modules/**",
+  "node_modules",
+  "**/*.d.ts",
+  "**/*.min.*",
+  "**/*-min.*",
+  "**/*_min.*",
+  "**/.yarn/**",
+  "**/.temp/**",
+  "**/.tmp/**",
+  "**/.next/**",
+  "**/.nuxt/**",
+  "**/.output/**",
+  "**/.idea/**",
+  "**/.history/**",
+  "**/.cache/**",
+  "**/coverage/**",
+  "**/build/**",
+];
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct Ignore(pub Vec<String>);
+
+impl Default for Ignore {
+  fn default() -> Self {
+    Self(IGNORE_PATTERNS.iter().map(|s| s.to_string()).collect())
+  }
+}
+
+impl Deref for Ignore {
+  type Target = Vec<String>;
+
+  fn deref(&self) -> &Self::Target {
+    &self.0
+  }
+}
+
+impl DerefMut for Ignore {
+  fn deref_mut(&mut self) -> &mut Self::Target {
+    &mut self.0
+  }
+}
+
+impl From<Vec<String>> for Ignore {
+  fn from(value: Vec<String>) -> Self {
+    Self(value)
+  }
+}
--- crates/core/src/lib.rs
+++ crates/core/src/lib.rs
@@ -1,7 +1,9 @@
 mod error;
+mod ignore;
 pub mod loc;
 mod message;
 
 pub mod traits;
 pub use error::*;
+pub use ignore::*;
 pub use message::*;
