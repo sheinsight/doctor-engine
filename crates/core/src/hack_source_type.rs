bbf9d48dabf9be2a3135b1199e078438b55139c0
dr2009 <dr2009.china@gmail.com>
2025-09-19 09:54:09 +0800

    fix: 语法检测增加对 .mts 和 .cts 支持

--- crates/core/src/hack_source_type.rs
+++ crates/core/src/hack_source_type.rs
@@ -3,9 +3,13 @@ use oxc::span::SourceType;
 pub fn hack_source_type_from_path(path: &std::path::Path) -> SourceType {
   match path.extension().and_then(|ext| ext.to_str()) {
     Some("ts") => SourceType::ts(),
+    Some("mts") => SourceType::ts(),
+    Some("cts") => SourceType::ts(),
     Some("tsx") => SourceType::tsx(),
-    Some("jsx") => SourceType::jsx(),
+    // Some("js") => SourceType::mjs(),
+    Some("mjs") => SourceType::mjs(),
     Some("cjs") => SourceType::cjs(),
+    Some("jsx") => SourceType::jsx(),
     _ => SourceType::jsx(),
   }
 }
---
+++ crates/syntax/fixtures/a.mts
@@ -0,0 +1,7 @@
+
+
+type A = Record<string, number>;
+
+const a: A = { foo: 1, bar: 2 };
+
+console.log(a);
